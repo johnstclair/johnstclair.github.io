aba23238ba87296c31596ae8d5e2446d50a44ec0
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-04-16 22:28:02 -0700

    feat: adaptive grid

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 import "../styles/background.css"
@@ -6,16 +7,34 @@ interface props {
   children: JSX.Element,
 }
 
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
+
 function Background({ children }: props ) {
+  useWindowSize()
+
   return (
     <>
       <div className="background-container">
-        {Array.from({ length: 250 }).map((_, i) => {
-          return (<motion.div
-            key={i}
-            className="background-grid-item"
-        ></motion.div>
-      )})}
+        {Array.from({ length: Math.floor(document.body.clientHeight / 100) }).map(_ => {
+          return (<>
+            {Array.from({ length: Math.floor(document.body.clientWidth / 100) }).map(_ => {
+              document.documentElement.style.setProperty('--background-cols', `${Math.floor(document.body.clientWidth / 100)}`);
+              document.documentElement.style.setProperty('--background-rows', `${Math.floor(document.body.clientHeight / 100)}`);
+              return <motion.div className="background-grid-item" ></motion.div>
+            })}
+          </>) 
+        })}
       </div>
       <main>
         {children}
