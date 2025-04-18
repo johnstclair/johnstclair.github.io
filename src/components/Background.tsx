c12ada45296e3763d678f56484e8a923597742f9
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-04-17 20:24:41 -0700

    feat: mobile responsive

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 import "../styles/Background.css"
@@ -21,16 +21,34 @@ function useWindowSize() {
 }
 
 function Background({ children }: props ) {
+  const [effectRunner, setEffectRunner] = useState<number>(3);
+
   useWindowSize()
+
+  document.documentElement.style.setProperty('--background-cols', `${Math.floor(document.body.scrollWidth / 100)}`);
+  document.documentElement.style.setProperty('--background-rows', `${Math.floor(document.body.scrollHeight / 100)}`);
+
+  document.documentElement.style.setProperty('--scroll-width', `${document.body.scrollWidth}px`);
+  document.documentElement.style.setProperty('--scroll-height', `${document.body.scrollHeight}px`);
+
+  console.log(`${document.body.scrollHeight}h by ${document.body.scrollWidth}w`)
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+
+      if (effectRunner != 0) {
+        setEffectRunner(effectRunner-1);
+      }
+    }, 125);
+  }, [effectRunner])
 
   return (
     <>
       <div className="background-container">
-        {Array.from({ length: Math.floor(document.body.clientHeight / 100) }).map(() => {
+        {Array.from({ length: Math.floor(document.body.scrollHeight / 100) }).map(() => {
           return (<>
-            {Array.from({ length: Math.floor(document.body.clientWidth / 100) }).map(() => {
-              document.documentElement.style.setProperty('--background-cols', `${Math.floor(document.body.clientWidth / 100)}`);
-              document.documentElement.style.setProperty('--background-rows', `${Math.floor(document.body.clientHeight / 100)}`);
+            {Array.from({ length: Math.floor(document.body.scrollWidth / 100) }).map(() => {
               return <motion.div 
                 whileTap={{scale: 1.2}}
                 onClick={() => {
